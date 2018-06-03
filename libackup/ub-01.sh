b58077301a1d2d60755f566d06bbffa6a62d3946
hikkiii <gdieie@163.com>
2018-06-04 00:34:15 +0800

    Update ub-01.sh

--- libackup/ub-01.sh
+++ libackup/ub-01.sh
@@ -4,7 +4,8 @@ sudo rm /etc/issue;sudo rm /etc/issue.net;sudo rm /etc/redhat-release;sudo rm /e
 sudo echo 1 > /proc/sys/net/ipv4/tcp_syncookies #TCP SYN Cookie protect
 
 sudo apt update -y
-sudo apt remove pump apmd isapnptools redhat-logos mt-st kernel-pcmcia-cs setserial redhat-release sudo apt remove eject linuxconf kudzu gd bc getty_ps raidtools pciutils mailcap setconsole gnupg
+sudo apt remove pump apmd isapnptools redhat-logos mt-st kernel-pcmcia-cs setserial redhat-release 
+sudo apt remove eject linuxconf kudzu gd bc getty_ps raidtools pciutils mailcap setconsole gnupg samba* 
 sudo apt install w3m links lynx zsh unzip jq net-tools vim goldendict -y
 sudo apt remove cups -y
 sudo userdel adm
